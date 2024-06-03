d333759d19ed45b56ec2635bc8231f97aa47aa0c
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-06-03 04:43:08 +0100

    /c/<text>: display “C ” followed by the value of the text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+start Flask application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the text variable"""
+    parsed_text = text.replace('_', ' ')
+    return "C {}".format(parsed_text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
